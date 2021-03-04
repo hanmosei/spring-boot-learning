be08afc7c87ede4ecd72ca3af3157284f12022ae
陈泰 <chentay@aliyun.com>
2021-03-04 11:04:20 +0800

    重构项目

--- src/main/java/com/hms/ct/SpringBootLearningApplication.java
+++ src/main/java/com/hms/ct/SpringBootLearningApplication.java
@@ -1,15 +1,13 @@
-package com.hms.learn;
+package com.hms.ct;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
-@EnableCaching
 @SpringBootApplication
-public class Application {
+public class SpringBootLearningApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(SpringBootLearningApplication.class, args);
     }
 
 }
--- src/test/java/com/hms/ct/SpringBootLearningApplicationTests.java
+++ src/test/java/com/hms/ct/SpringBootLearningApplicationTests.java
@@ -1,10 +1,10 @@
-package com.hms.learn;
+package com.hms.ct;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class SpringBootLearningApplicationTests {
 
     @Test
     void contextLoads() {
